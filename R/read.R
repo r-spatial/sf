c776d5df8d14c8cbb063a5d191f604bd046a8143
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2016-07-24 13:13:15 +0200

    clean up read.sf

--- R/read.R
+++ R/read.R
@@ -1,47 +1,49 @@
+# read feature id from layer:
+readFeature = function(layer, id) {
+	# xylst2mtrx cbinds list(x=...,y=...) instances to matrix in nested lists:
+	xylst2mtrx = function(x) 
+		if (all(c("x", "y") %in% names(x))) # we're at the deepest level
+			do.call(cbind, x) 
+		else 
+			lapply(x, xylst2mtrx)
+	ft = rgdal2::getFeature(layer, id)
+	geom = rgdal2::getGeometry(ft)
+	pts = xylst2mtrx(rgdal2::getPoints(geom, nested = TRUE))
+	switch (rgdal2::getGeometryType(geom),
+      	POINT25D = ,
+       	POINT = ST_Point(unlist(pts)),
+      	LINESTRING25D = ,
+       	LINESTRING = ST_LineString(pts),
+       	MULTILINESTRING = ST_MultiLineString(pts),
+       	POLYGON25D = ,
+       	POLYGON = ST_Polygon(pts),
+       	MULTIPOLYGON25D = ,
+       	MULTIPOLYGON = ST_MultiPolygon(pts),
+       	MULTIPOINT = ,
+       	MULTIPOINT25D = ST_MultiPoint(pts),
+       	LINEARRING = ,
+       	MULTILINESTRING25D = ,
+			stop(paste("geometry type", rgdal2::getGeometryType(geom), "not supported"))
+	)
+}
+
 #' read simple features from file or database
 #'
 #' read simple features from file or database
 #' @param dsn data source name (interpretation varies by driver - for some drivers, dsn is a file name, but may also be a folder)
 #' @param layer layer name (varies by driver, may be a file name without extension)
+#' @param ... parameter(s) passed on to \link{ST_as.sf}
 #' @examples
 #' if (Sys.getenv("USER") == "edzer") {
 #'  (s = read.sf("PG:dbname=test", "test"))
 #'  summary(s)
 #' }
-#' if (require(rgdal2)) {
-#'  (s = read.sf(system.file("shapes/", package="maptools"), "sids"))[1:10,]
-#'  summary(s)
-#' }
+#' (s = read.sf(system.file("shapes/", package="maptools"), "sids"))[1:10,]
+#' summary(s)
 #' @export
-read.sf = function(dsn, layer) {
+read.sf = function(dsn, layer, ...) {
 	if (!requireNamespace("rgdal2", quietly = TRUE))
 		stop("package rgdal2 required for read.sf; try devtools::install_github(\"edzer/rgdal2\")")
-	flatten = function(x)
-		if (all(c("x", "y") %in% names(x))) # we're at the deepest level
-			do.call(cbind, x) 
-		else 
-			lapply(x, flatten)
-	readFeature = function(layer, id) {
-		ft = rgdal2::getFeature(layer, id)
-		geom = rgdal2::getGeometry(ft)
-		pts = flatten(rgdal2::getPoints(geom, nested = TRUE))
-		switch (rgdal2::getGeometryType(geom),
-           	POINT25D = ,
-           	POINT = ST_Point(unlist(pts)),
-           	LINESTRING25D = ,
-           	LINESTRING = ST_LineString(pts),
-           	MULTILINESTRING = ST_MultiLineString(pts),
-           	POLYGON25D = ,
-           	POLYGON = ST_Polygon(pts),
-           	MULTIPOLYGON25D = ,
-           	MULTIPOLYGON = ST_MultiPolygon(pts),
-           	MULTIPOINT = ,
-           	MULTIPOINT25D = ST_MultiPoint(pts),
-           	LINEARRING = ,
-           	MULTILINESTRING25D = ,
-				stop(paste(rgdal2::getGeometryType(geom), "not supported"))
-		)
-	}
 	o = rgdal2::openOGRLayer(dsn, layer)
 	ids = rgdal2::getIDs(o)
 	srs = rgdal2::getSRS(o)
@@ -50,5 +52,5 @@ read.sf = function(dsn, layer) {
 	f = lapply(ids, function(id) rgdal2::getFields(rgdal2::getFeature(o, id)))
 	df = data.frame(row.names = ids, apply(do.call(rbind, f), 2, unlist))
 	df$geom = geom
-	ST_as.sf(df)
+	ST_as.sf(df, ...)
 }
