ddf16e527319a3a0edbf19792d055b9b6b6a679d
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2016-08-24 14:41:47 +0200

    add cov tests

---
+++ tests/testthat/test_sfi.R
@@ -0,0 +1,39 @@
+context("sf")
+
+test_that("MtrxSet is being called", {
+  outer = matrix(c(0,0,10,0,10,10,0,10,0,0),ncol=2, byrow=TRUE)
+  hole1 = matrix(c(1,1,1,2,2,2,2,1,1,1),ncol=2, byrow=TRUE)
+  hole2 = matrix(c(5,5,5,6,6,6,6,5,5,5),ncol=2, byrow=TRUE)
+  pts = list(outer, hole1, hole2)
+  pl1 = st_polygon(pts)
+  expect_identical(st_as_wkt(pl1),
+  "POLYGON((0 0, 10 0, 10 10, 0 10, 0 0), (1 1, 1 2, 2 2, 2 1, 1 1), (5 5, 5 6, 6 6, 6 5, 5 5))")
+})
+
+test_that("Dimension works", {
+  expect_identical(sf:::Dimension(st_point(1:2)), "XY")
+  expect_identical(sf:::Dimension(st_point(1:3)), "XYZ")
+  expect_identical(sf:::Dimension(st_point(1:3, "XYZ")), "XYZ")
+  expect_identical(sf:::Dimension(st_point(1:3, "XYM")), "XYM")
+  expect_identical(sf:::Dimension(st_point(1:4)), "XYZM")
+})
+
+test_that("st_multilinestring works", {
+  outer = matrix(c(0,0,10,0,10,10,0,10,0,0),ncol=2, byrow=TRUE)
+  hole1 = matrix(c(1,1,1,2,2,2,2,1,1,1),ncol=2, byrow=TRUE)
+  hole2 = matrix(c(5,5,5,6,6,6,6,5,5,5),ncol=2, byrow=TRUE)
+  pts = list(outer, hole1, hole2)
+  ml1 = st_multilinestring(pts)
+  expect_identical(st_as_wkt(ml1), "MULTILINESTRING((0 0, 10 0, 10 10, 0 10, 0 0), (1 1, 1 2, 2 2, 2 1, 1 1), (5 5, 5 6, 6 6, 6 5, 5 5))")
+})
+
+test_that("xx2multixx works", {
+  expect_identical(sf:::POINT2MULTIPOINT(st_point(1:2)), st_multipoint(matrix(1:2,1)))
+  m = matrix(1:6,,2)
+  expect_identical(sf:::LINESTRING2MULTILINESTRING(st_linestring(m)), st_multilinestring(list(m)))
+  outer = matrix(c(0,0,10,0,10,10,0,10,0,0),ncol=2, byrow=TRUE)
+  hole1 = matrix(c(1,1,1,2,2,2,2,1,1,1),ncol=2, byrow=TRUE)
+  hole2 = matrix(c(5,5,5,6,6,6,6,5,5,5),ncol=2, byrow=TRUE)
+  pts = list(outer, hole1, hole2)
+  expect_identical(sf:::POLYGON2MULTIPOLYGON(st_polygon(pts)), st_multipolygon(list(pts)))
+})
