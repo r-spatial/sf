fbbde07dc18968dd077959c28fdbe7b7a7e2a472
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2016-09-07 12:02:05 +0200

    add header file and geos test

---
+++ src/wkb.h
@@ -0,0 +1,4 @@
+
+Rcpp::List ReadWKB(Rcpp::List wkb_list, bool EWKB, int endian, bool debug);
+Rcpp::List WriteWKB(Rcpp::List sfc, bool EWKB, int endian, Rcpp::CharacterVector dim, 
+	bool debug);
---
+++ tests/testthat/test_geos.R
@@ -0,0 +1,19 @@
+context("sf: geos tests")
+
+test_that("st_g_relate works", {
+  r1 = sf:::st_g_relate(st_sfc(list(st_point(c(0,0)))), st_sfc(list(st_linestring(rbind(c(0,0),c(1,1))))))
+  library(sp)
+  p = SpatialPoints(matrix(0,1,2))
+  l = Lines(list(Line(rbind(c(0,0),c(1,1)))), "ID")
+  sl = SpatialLines(list(l))
+  library(rgeos)
+  r2 = gRelate(p, sl)
+  expect_true(all(r1 == r2))
+})
+
+test_that("st_g_isValid works", {
+  expect_true( sf:::st_g_isValid(
+  	st_sfc(list(st_polygon(list(cbind(c(0,1,1,0,0), c(0,0,1, 1,0))))))))
+  expect_false(sf:::st_g_isValid(
+  	st_sfc(list(st_polygon(list(cbind(c(0,1,1,.5,0),c(0,0,1,-1,0))))))))
+})
