a2c3d3deae1492fd7a39c546b4caf06d8b8e177d
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2016-09-22 16:47:30 +0200

    write tests

---
+++ tests/write.R
@@ -0,0 +1,11 @@
+library(sp)
+demo(meuse, ask = FALSE, echo = FALSE)
+library(sf)
+meuse = st_as_sf(meuse)
+# write shapefile:
+st_write(meuse, ".", "meuse")
+library(RPostgreSQL)
+cn = dbConnect(PostgreSQL(), dbname = "postgis")
+st_write_db(cn, meuse, "meuse", dropTable = FALSE)
+st_write_db(cn, meuse, "meuse", dropTable = TRUE, wkb = FALSE)
+dbDisconnect(cn)
