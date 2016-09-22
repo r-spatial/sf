198840c56b3c0563edcacd76ccb4173771a32ec1
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2016-09-22 22:49:41 +0200

    check appv.

--- tests/write.R
+++ tests/write.R
@@ -5,7 +5,8 @@ meuse = st_as_sf(meuse)
 # write shapefile:
 st_write(meuse, ".", "meuse")
 library(RPostgreSQL)
-cn = dbConnect(PostgreSQL(), dbname = "postgis")
+# -- doesn't work on appveyor:
+#cn = dbConnect(PostgreSQL(), dbname = "postgis")
 #st_write_db(cn, meuse, "meusedb", dropTable = FALSE)
 #st_write_db(cn, meuse, "meusedb", dropTable = TRUE, wkb = FALSE)
-dbDisconnect(cn)
+#dbDisconnect(cn)
