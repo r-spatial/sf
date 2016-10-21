992ddea031f5b3db32d8e7be39f6d0b16cd0f629
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2016-10-21 11:33:36 +0200

    add tests

--- tests/testthat/test_sfc.R
+++ tests/testthat/test_sfc.R
@@ -8,3 +8,20 @@ test_that("we can print sfc objects", {
   print(st_sfc())
   expect_equal(length(st_sfc()), 0)
 })
+
+test_that("st_is_longlat works", {
+  pt1 = st_point(1:2)
+  pt2 = st_point(3:4)
+  s1 = st_sf(a = c("x", "y"), geom = st_sfc(pt1, pt2))
+  expect_equal(st_is_longlat(s1), NA)
+  s2 = s1
+  st_crs(s2) = 3857
+  expect_false(st_is_longlat(s2))
+  st_crs(s1) = 4326
+  expect_true(st_is_longlat(s1))
+})
+
+test_that("st_crs returns NA for sfg objects", {
+  pt1 = st_point(1:2)
+  expect_true(is.na(st_crs(pt1)))
+})
