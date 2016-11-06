be7c3884ca386a3086d79be4eb308aa918a12bf6
Kendon Bell <kmb56@berkeley.edu>
2016-11-06 14:56:49 -0800

    Added tests for stringsAsFactors

--- tests/testthat/test_read.R
+++ tests/testthat/test_read.R
@@ -5,3 +5,15 @@ test_that("we can read a shapefile using st_read", {
   expect_identical(class(nc), c("sf", "data.frame"))
   expect_equal(dim(nc), c(100, 15))
 })
+
+test_that("stringsAsFactors = FALSE produces a data.frame with no factors", {
+  nc <-  st_read(system.file("shape/nc.shp", package="sf"), "nc", crs = 4267,
+                 stringsAsFactors = FALSE)
+  expect_false(any(sapply(nc, class) == "factor"))
+})
+
+test_that("stringsAsFactors = TRUE produces a data.frame with factors", {
+  nc <-  st_read(system.file("shape/nc.shp", package="sf"), "nc", crs = 4267,
+                 stringsAsFactors = TRUE)
+  expect_true(any(sapply(nc, class) == "factor"))
+})
