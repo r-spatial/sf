25fbda6be6412850f118c429314c5fc1958d4b25
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2016-12-09 21:46:33 +0100

    fix crs

--- demo/meuse_sf.R
+++ demo/meuse_sf.R
@@ -1,5 +1,5 @@
 data(meuse, package = "sp") # load data.frame from sp
 library(sf)
-meuse_sf = st_as_sf(meuse, coords = c("x", "y"), epsg = 28992)
+meuse_sf = st_as_sf(meuse, coords = c("x", "y"), crs = 28992)
 meuse_sf[1:5,]
 summary(meuse_sf[1:5,])
