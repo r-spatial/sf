df51c269301f3b0b3ff348eec150447d34dff9f8
etiennebr <etiennebr@gmail.com>
2016-12-21 15:24:17 -0500

    silence tests

--- tests/testthat/test_read.R
+++ tests/testthat/test_read.R
@@ -1,20 +1,20 @@
 context("sf: read tests")
 
 test_that("we can read a shapefile using st_read", {
-  nc = st_read(system.file("shape/nc.shp", package="sf"), "nc", crs = 4267)
+  nc <- st_read(system.file("shape/nc.shp", package="sf"), "nc", crs = 4267, quiet = TRUE)
   expect_identical(class(nc), c("sf", "data.frame"))
   expect_equal(dim(nc), c(100, 15))
 })
 
 test_that("stringsAsFactors = FALSE produces a data.frame with no factors", {
   nc <-  st_read(system.file("shape/nc.shp", package="sf"), "nc", crs = 4267,
-                 stringsAsFactors = FALSE)
+                 stringsAsFactors = FALSE, quiet = TRUE)
   expect_false(any(sapply(nc, class) == "factor"))
 })
 
 test_that("stringsAsFactors = TRUE produces a data.frame with factors", {
   nc <-  st_read(system.file("shape/nc.shp", package="sf"), "nc", crs = 4267,
-                 stringsAsFactors = TRUE)
+                 stringsAsFactors = TRUE, quiet = TRUE)
   expect_true(any(sapply(nc, class) == "factor"))
 })
 
