9c9df72b352847c589627b6107f6e6b1cc69e14d
Farid <faridcher@gmail.com>
2017-01-04 03:52:56 -0500

    cbind.sf

--- R/sf.R
+++ R/sf.R
@@ -249,7 +249,7 @@ print.sf = function(x, ..., n =
 #' @export
 rbind.sf = function(..., deparse.level = 1) {
 	ret = base::rbind.data.frame(...)
-	st_geometry(ret) = do.call(st_sfc, c(st_geometry(ret),crs=st_crs(st_geometry(ret))$epsg))
+	st_geometry(ret) = do.call(st_sfc, list(st_geometry(ret),crs=st_crs(ret)))
 	ret
 }
 
