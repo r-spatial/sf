01f3da7f10d64cc50fa1533830b3605a4ae032e7
etiennebr <etiennebr@gmail.com>
2017-01-05 20:16:15 -0500

    test shapefile write within test/ dir
    #127

--- tests/testthat/test_tm.R
+++ tests/testthat/test_tm.R
@@ -5,10 +5,11 @@ test_that("st_read and write handle date and time", {
   Sys.setenv(TZ="UTC")
   x = st_sf(a = 1:2, b=c(5.6,3), dt = Sys.Date()+1:2, tm = Sys.time()+2:3, 
 	  geometry = st_sfc(st_point(c(1,1)),st_point(c(2,2))))
-  shp <- paste0(tempfile(), ".shp")
+  shp <- paste0("test_shapefile", c(".shp", ".shx", ".dbf"))
   gpkg <- paste0(tempfile(), ".gpkg")
-  st_write(x[-4], shp, quiet = TRUE)
-  x2 = st_read(shp, quiet = TRUE)
+  on.exit(file.remove(shp))
+  st_write(x[-4], shp[1], quiet = TRUE)
+  x2 = st_read(shp[1], quiet = TRUE)
   expect_equal(x[-4], x2)
   if (Sys.getenv("USER") %in% c("edzer", "travis")) { # 
      x = st_sf(a = 1:2, b=c(5.6,3), dt = Sys.Date()+1:2, tm = Sys.time()+2:3, 
