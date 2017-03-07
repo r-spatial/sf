0abe9cf5f1cec51d43827a618d815edf60d78550
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2017-03-07 19:47:45 +0100

    add proper st_make_valid methods

--- R/lwgeom.R
+++ R/lwgeom.R
@@ -10,16 +10,16 @@ st_make_valid = function(x) UseMethod("st_make_valid")
 
 #' @export
 st_make_valid.sfg = function(x) {
-	st_sfc(CPL_make_valid(st_geometry(x)))[[1]]
+	st_make_valid(st_geometry(x))[[1]]
 }
 
 #' @export
 st_make_valid.sfc = function(x) {
-	st_sfc(CPL_make_valid(x))
+	st_sfc(CPL_make_valid(x), crs = st_crs(x))
 }
 
 #' @export
-st_make_valid.sfg = function(x) {
+st_make_valid.sf = function(x) {
 	st_geometry(x) = st_make_valid(st_geometry(x))
 	x
 }
