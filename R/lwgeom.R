796a681899882f342b67b43eaa53b19027c34f52
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2017-03-16 09:29:02 +0100

    add example to st_make_valid

--- R/lwgeom.R
+++ R/lwgeom.R
@@ -4,6 +4,12 @@
 #' @param x object of class \code{sfg}, \code{sfg} or \code{sf}
 #' @return object of the same class as \code{x}
 #' @details uses the \code{lwgeom_makevalid} method also used by PostGIS' \code{ST_makevalid}.
+#' @examples
+#' x = st_sfc(st_polygon(list(rbind(c(0,0),c(0.5,0),c(0.5,0.5),c(0.5,0),c(1,0),c(1,1),c(0,1),c(0,0)))))
+#' st_is_valid(x)
+#' y = st_make_valid(x)
+#' st_is_valid(y)
+#' y %>% st_cast()
 #' @export
 st_make_valid = function(x) UseMethod("st_make_valid")
 
