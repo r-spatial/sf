b846c86f9293132b74999959288e536f95922be0
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2017-03-27 21:12:36 +0200

    add nc test

--- tests/testthat/test_write.R
+++ tests/testthat/test_write.R
@@ -10,11 +10,12 @@ test_that("sf can write to all writable formats", {
     drvs <- st_drivers()$name[sapply(st_drivers()$name, function(x) is_driver_can(x, operation = "write"))] %>% as.character()
     excluded_drivers = c("gps", # requires options
                          "gtm", # doesn't handle attributes
-                         "nc",  # requires appropriate datum
+                         "nc",  # requires appropriate datum -> writes in 4326, below
                          "map") # doesn't support points
     for (ext in setdiff(names(extension_map[extension_map %in% drvs]),
                         excluded_drivers))
         st_write(meuse, paste0(tf, ".", ext))
+	st_write(st_transform(meuse, st_crs(4326)), paste0(tf, ".nc"))
 })
 
 test_that("sf can write units (#264)", {
