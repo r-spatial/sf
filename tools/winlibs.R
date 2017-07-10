09db3f9cf6e1419a8c83db8363542ae643116eb9
Jeroen Ooms <jeroenooms@gmail.com>
2017-07-10 11:17:52 -0700

    Update Windows to GDAL 2.2.0 stack

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -2,9 +2,9 @@ if(getRversion() < "3.3.0") {
   stop("Your version of R is too old. This package requires R-3.3.0 or newer on Windows.")
 }
 
-# Download gdal-2.0.1 from rwinlib
-if(!file.exists("../windows/gdal2-2.1.1/include/gdal/gdal.h")){
-  download.file("https://github.com/rwinlib/gdal2/archive/v2.1.1.zip", "lib.zip", quiet = TRUE)
+# Download gdal-2.2.0 from rwinlib
+if(!file.exists("../windows/gdal2-2.2.0/include/gdal/gdal.h")){
+  download.file("https://github.com/rwinlib/gdal2/archive/v2.2.0.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
