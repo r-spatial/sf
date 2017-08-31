f5edd456778818173015502456e4c28a1e4354a1
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2017-08-31 16:42:19 +0200

    use Rcpp for native routine registration

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -586,3 +586,60 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+
+static const R_CallMethodDef CallEntries[] = {
+    {"_sf_CPL_get_bbox", (DL_FUNC) &_sf_CPL_get_bbox, 2},
+    {"_sf_CPL_gdal_init", (DL_FUNC) &_sf_CPL_gdal_init, 0},
+    {"_sf_CPL_gdal_cleanup_all", (DL_FUNC) &_sf_CPL_gdal_cleanup_all, 0},
+    {"_sf_CPL_gdal_version", (DL_FUNC) &_sf_CPL_gdal_version, 1},
+    {"_sf_CPL_crs_parameters", (DL_FUNC) &_sf_CPL_crs_parameters, 1},
+    {"_sf_CPL_crs_from_epsg", (DL_FUNC) &_sf_CPL_crs_from_epsg, 1},
+    {"_sf_CPL_crs_from_wkt", (DL_FUNC) &_sf_CPL_crs_from_wkt, 1},
+    {"_sf_CPL_roundtrip", (DL_FUNC) &_sf_CPL_roundtrip, 1},
+    {"_sf_CPL_circularstring_to_linestring", (DL_FUNC) &_sf_CPL_circularstring_to_linestring, 1},
+    {"_sf_CPL_multisurface_to_multipolygon", (DL_FUNC) &_sf_CPL_multisurface_to_multipolygon, 1},
+    {"_sf_CPL_compoundcurve_to_linear", (DL_FUNC) &_sf_CPL_compoundcurve_to_linear, 1},
+    {"_sf_CPL_curve_to_linestring", (DL_FUNC) &_sf_CPL_curve_to_linestring, 1},
+    {"_sf_CPL_transform", (DL_FUNC) &_sf_CPL_transform, 2},
+    {"_sf_CPL_wrap_dateline", (DL_FUNC) &_sf_CPL_wrap_dateline, 3},
+    {"_sf_CPL_crs_from_proj4string", (DL_FUNC) &_sf_CPL_crs_from_proj4string, 1},
+    {"_sf_CPL_get_rgdal_drivers", (DL_FUNC) &_sf_CPL_get_rgdal_drivers, 1},
+    {"_sf_CPL_sfc_from_wkt", (DL_FUNC) &_sf_CPL_sfc_from_wkt, 1},
+    {"_sf_CPL_area", (DL_FUNC) &_sf_CPL_area, 1},
+    {"_sf_CPL_gdal_dimension", (DL_FUNC) &_sf_CPL_gdal_dimension, 2},
+    {"_sf_CPL_length", (DL_FUNC) &_sf_CPL_length, 1},
+    {"_sf_CPL_gdal_segmentize", (DL_FUNC) &_sf_CPL_gdal_segmentize, 2},
+    {"_sf_CPL_gdal_linestring_sample", (DL_FUNC) &_sf_CPL_gdal_linestring_sample, 2},
+    {"_sf_CPL_get_layers", (DL_FUNC) &_sf_CPL_get_layers, 3},
+    {"_sf_CPL_read_ogr", (DL_FUNC) &_sf_CPL_read_ogr, 7},
+    {"_sf_CPL_write_ogr", (DL_FUNC) &_sf_CPL_write_ogr, 12},
+    {"_sf_CPL_geos_binop", (DL_FUNC) &_sf_CPL_geos_binop, 7},
+    {"_sf_CPL_geos_is_valid_reason", (DL_FUNC) &_sf_CPL_geos_is_valid_reason, 1},
+    {"_sf_CPL_geos_is_valid", (DL_FUNC) &_sf_CPL_geos_is_valid, 2},
+    {"_sf_CPL_geos_is_simple", (DL_FUNC) &_sf_CPL_geos_is_simple, 1},
+    {"_sf_CPL_geos_union", (DL_FUNC) &_sf_CPL_geos_union, 2},
+    {"_sf_CPL_geos_op", (DL_FUNC) &_sf_CPL_geos_op, 8},
+    {"_sf_CPL_geos_voronoi", (DL_FUNC) &_sf_CPL_geos_voronoi, 4},
+    {"_sf_CPL_geos_op2", (DL_FUNC) &_sf_CPL_geos_op2, 3},
+    {"_sf_CPL_geos_version", (DL_FUNC) &_sf_CPL_geos_version, 1},
+    {"_sf_CPL_geos_dist", (DL_FUNC) &_sf_CPL_geos_dist, 2},
+    {"_sf_CPL_geos_relate", (DL_FUNC) &_sf_CPL_geos_relate, 2},
+    {"_sf_CPL_invert_sparse_incidence", (DL_FUNC) &_sf_CPL_invert_sparse_incidence, 2},
+    {"_sf_CPL_lwgeom_version", (DL_FUNC) &_sf_CPL_lwgeom_version, 1},
+    {"_sf_CPL_make_valid", (DL_FUNC) &_sf_CPL_make_valid, 1},
+    {"_sf_CPL_split", (DL_FUNC) &_sf_CPL_split, 2},
+    {"_sf_CPL_geohash", (DL_FUNC) &_sf_CPL_geohash, 2},
+    {"_sf_CPL_proj_version", (DL_FUNC) &_sf_CPL_proj_version, 1},
+    {"_sf_CPL_proj_is_valid", (DL_FUNC) &_sf_CPL_proj_is_valid, 1},
+    {"_sf_CPL_proj_info", (DL_FUNC) &_sf_CPL_proj_info, 1},
+    {"_sf_CPL_hex_to_raw", (DL_FUNC) &_sf_CPL_hex_to_raw, 1},
+    {"_sf_CPL_raw_to_hex", (DL_FUNC) &_sf_CPL_raw_to_hex, 1},
+    {"_sf_CPL_read_wkb", (DL_FUNC) &_sf_CPL_read_wkb, 4},
+    {"_sf_CPL_write_wkb", (DL_FUNC) &_sf_CPL_write_wkb, 5},
+    {NULL, NULL, 0}
+};
+
+RcppExport void R_init_sf(DllInfo *dll) {
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
