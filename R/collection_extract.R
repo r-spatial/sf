1479cc96063f67ef24c138755764e82fdf04c35f
ateucher <andy.teucher@gmail.com>
2017-09-13 11:54:35 -0700

    Use inherits instead of is to match rest of pkg

--- R/collection_extract.R
+++ R/collection_extract.R
@@ -18,7 +18,7 @@ st_collectionextract.sfg = function(x, type = c("POLYGON", "POINT", "LINESTRING"
 	type = match.arg(type)
 	types = c(type, paste0("MULTI", type))
 
-	if (is(x, types)) {
+	if (inherits(x, types)) {
 		warning("x is already of type ", type, ".")
 		return(x)
 	}
@@ -38,13 +38,13 @@ st_collectionextract.sfc = function(x, type = c("POLYGON", "POINT", "LINESTRING"
 	type = match.arg(type)
 	types = c(type, paste0("MULTI", type))
 
-	if (is(st_geometry(x), paste0("sfc_", types))) {
+	if (inherits(st_geometry(x), paste0("sfc_", types))) {
 		warning("x is already of type ", type, ".")
 		return(x)
 	}
 
 	# Cast to GEOMETRYCOLLECTION if not already (e.g., if it is sfc_GEOMETRY)
-	if (!is(st_geometry(x), "sfc_GEOMETRYCOLLECTION")) {
+	if (!inherits(st_geometry(x), "sfc_GEOMETRYCOLLECTION")) {
 		x = st_cast(x, "GEOMETRYCOLLECTION")
 	}
 
@@ -52,7 +52,7 @@ st_collectionextract.sfc = function(x, type = c("POLYGON", "POINT", "LINESTRING"
 	gc_casted = st_cast(x)
 
 	## Keep only components that match input type
-	if (is(gc_casted, "sf")) {
+	if (inherits(gc_casted, "sf")) {
 		gc_types = gc_casted[st_is(gc_casted, types), ]
 	} else {
 		gc_types = gc_casted[st_is(gc_casted, types)]
