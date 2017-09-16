fbcb548d8bdf8cad72eea127a8af51fff2a8f412
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2017-09-16 11:38:27 +0200

    add st_jitter; see #483

---
+++ R/jitter.R
@@ -0,0 +1,28 @@
+
+#' jitter geometries
+#' @param x object of class \code{sf} or \code{sfc}
+#' @param amount amount of jittering applied; if missing, the amount is set to factor * the bounding box diagonal.
+#' @details jitters coordinates with an amount such that `code{runif(1, -amount, amount)} is added to the coordinates. x- and y-coordinates are jittered independently but all coordinates of a single geometry are jittered with the same amount, meaning that the geometry shape does not change. For longlat data, a latitude correction is made such that jittering in East and North directions are identical in distance in the center of the bounding box of \code{x}.
+#' @examples
+#' nc = read_sf(system.file("gpkg/nc.gpkg", package="sf"))
+#' pts = st_centroid(st_geometry(nc))
+#' plot(pts)
+#' plot(st_jitter(pts, .05), add = TRUE, col = 'red')
+#' plot(st_geometry(nc))
+#' plot(st_jitter(st_geometry(nc), factor = .01), add = T, col = '#ff8888')
+#' @export
+st_jitter = function(x, amount, factor = 0.002) {      
+	bb = st_bbox(x)
+	if (missing(amount))
+		amount = factor * sqrt(diff(bb[c(3,1)])^2 + diff(bb[c(4,2)])^2)
+	ay = amount
+	ax = if (st_is_longlat(x))
+		amount * cos(pi * mean(bb[c(2,4)]) / 180)
+	else
+		amount
+    f = function(z, amount_x, amount_y) {
+		c(runif(1L, -amount_x, amount_x),
+		  runif(1L, -amount_y, amount_y))
+	}
+	st_set_crs(x + lapply(x, f, amount_x = ax, amount_y = ay), st_crs(x))
+}
