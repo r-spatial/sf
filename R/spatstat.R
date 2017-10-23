408600e765ff3dbcaa976ec0f004962d7430e4f3
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2017-10-23 18:01:06 +0200

    add st_as_sf.ppp

--- R/spatstat.R
+++ R/spatstat.R
@@ -1,5 +1,35 @@
 #' @name st_as_sf
 #' @export
+#' @examples
+#' \dontrun{
+#'   require(spatstat)
+#'   g = st_as_sf(gorillas)
+#'   # select only the points:
+#'   g[st_is(g, "POINT"),]
+#' }
+st_as_sf.ppp = function(x, ...) {
+  if (!requireNamespace("spatstat", quietly = TRUE))
+    stop("package spatstat required, please install it first")
+  w = spatstat::edges(x$window)
+  mw = as.matrix(w$ends)
+  lst1 = lapply(seq_len(NROW(mw)), function(i) st_linestring(matrix(mw[i,], 2, byrow = TRUE)))
+  pol = st_cast(st_polygonize(do.call(c, do.call(st_sfc, lst1))), "POLYGON")
+  label = c("window")
+  winwork_sf = st_sf(label = label, geom = st_sfc(c(list(pol))))
+  m = as.matrix(data.frame(x$x, x$y))
+  pointwork = st_sfc(lapply(seq_len(NROW(m)), function(i) st_point(m[i,])))
+  points_sf = st_sf(label = rep("point", NROW(m)), geom = pointwork)
+  ret = rbind(winwork_sf, points_sf)
+  if (spatstat::is.marked(x)) {
+    m = spatstat::marks(x) 
+	cbind.sf(ret, m[c(NA, seq_len(nrow(m))),])
+  } else
+  	ret
+}
+
+
+#' @name st_as_sf
+#' @export
 st_as_sf.psp = function(x, ...) {
 	if (!requireNamespace("spatstat", quietly = TRUE))
 		stop("package spatstat required, please install it first")
--- tests/zzz.R
+++ tests/zzz.R
@@ -4,3 +4,8 @@ require(sf)
 data(chicago)
 st_as_sf(chicago)
 st_as_sf(chicago, both = FALSE)
+# ppp:
+g = gorillas
+st_as_sf(g)
+marks(g) = NULL
+st_as_sf(g)
