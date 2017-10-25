r-spatial/sf

commit 252c9250c78760eb03ee8c4259f93465f7a29c52

Unbreak plotting when column is all NA or has 1 value

R/plot.R
@@ -132,7 +132,7 @@ plot.sf <- function(x, y, ..., col = NULL, main, pal = NULL, nbreaks = 10, break
 				if (is.character(breaks)) { # compute breaks from values:
 					if (! requireNamespace("classInt", quietly = TRUE))
 						stop("package classInt required, please install it first")
-					breaks = if (length(values) > 1)
+					breaks = if (! all(is.na(values)) && length(unique(values)) > 1)
 						classInt::classIntervals(values, nbreaks, breaks)$brks
 					else
 						c(values, values) # lowest and highest!

commit 0f3c031b17dea8e39a804d45219b1134c59bd982

clean up self-caused mess in #528

R/plot.R
@@ -9,7 +9,7 @@
 #' @param breaks either a numeric vector with the actual breaks, or a name of a method accepted by the \code{style} argument of \link[classInt]{classIntervals}
 #' @param max.plot integer; lower boundary to maximum number of attributes to plot; the default value (9) can be overriden by setting the global option \code{sf_max.plot}, e.g. \code{options(sf_max.plot=2)}
 #' @param key.pos integer; which side to plot a color key: 1 bottom, 2 left, 3 top, 4 right. Set to \code{NULL} for no key. Currently ignored if multiple columns are plotted.
-#' @param key.size: amount of space reserved for the key (labels)
+#' @param key.size amount of space reserved for the key (labels)
 #' @param pch plotting symbol
 #' @param cex symbol size
 #' @param bg symbol background color
@@ -90,6 +90,7 @@ plot.sf <- function(x, y, ..., col = NULL, main, pal = NULL, nbreaks = 10, break
 		max.plot = if(is.null(n <- options("sf_max.plot")[[1]])) 9 else n, 
 		key.pos = if (ncol(x) > 2) NULL else 4, key.size = lcm(2.8)) {
 	stopifnot(missing(y))
+	breaks.missing = missing(breaks)
 	dots = list(...)
 
 	if (ncol(x) > 2 && !isTRUE(dots$add)) { # multiple maps to plot...
@@ -117,11 +118,13 @@ plot.sf <- function(x, y, ..., col = NULL, main, pal = NULL, nbreaks = 10, break
 		invisible(lapply(cols, function(cname) plot(x[, cname], main = cname, col = col,
 			pal = pal, nbreaks = nbreaks, breaks = breaks, key.pos = NULL, ...)))
 	} else { # single map, or dots$add=TRUE:
-		if (ncol(x) > 2)
-			stop("for plotting a single map, select a single attribute")
-		else if (is.null(col) && ncol(x) == 1) # no colors, no attributes to choose colors from: plot geometry
+		if (is.null(col) && ncol(x) == 1) # no colors, no attributes to choose colors from: plot geometry
 			plot(st_geometry(x), ...)
 		else { # generate plot with colors and possibly key
+			if (ncol(x) > 2) { # add = TRUE
+				warning("ignoring all but the first attribute")
+				x = x[,1]
+			}
 
 			# store attribute in "values":
 			values = x[[setdiff(names(x), attr(x, "sf_column"))]]
@@ -132,10 +135,12 @@ plot.sf <- function(x, y, ..., col = NULL, main, pal = NULL, nbreaks = 10, break
 			if (is.character(values))
 				values = as.factor(values)
 
-			if (is.null(col)) { # compute colors from values:
-				if (is.null(pal))
-					pal = function(n) sf.colors(n, categorical = is.factor(values))
+			if (is.null(pal))
+				pal = function(n) sf.colors(n, categorical = is.factor(values))
+			else if (! is.null(col))
+				stop("specify only one of col and pal")
 
+			if (is.null(col)) { # compute colors from values:
 				col = if (is.factor(values))
 						pal(nlevels(values))[as.numeric(values)]
 					else {
@@ -158,11 +163,12 @@ plot.sf <- function(x, y, ..., col = NULL, main, pal = NULL, nbreaks = 10, break
 								cut(as.numeric(values), breaks, include.lowest = TRUE)
 						pal(nbreaks)[cuts]
 					}
-			} else if (! is.null(pal))
-				stop("specify only one of col and pal")
-
+			} else # no key: # TODO: warn?
+				key.pos = NULL
+			
 			if (! is.null(key.pos) && !all(is.na(values)) &&
-					(is.factor(values) || length(unique(na.omit(values))) > 1)) { # plot key?
+					(is.factor(values) || length(unique(na.omit(values))) > 1) &&
+					length(col) > 1) { # plot key?
 				mar = c(1,1,1,1)
 				if (! is.factor(values))
 					mar[key.pos] = 3
@@ -171,7 +177,7 @@ plot.sf <- function(x, y, ..., col = NULL, main, pal = NULL, nbreaks = 10, break
 				if (key.pos %in% c(1,3))
 					mar[2] = 3
 				par(mar = mar)
-				on.exit(layout(1)) # set back
+				# on.exit(layout(1)) # set back
 				switch(key.pos,
 					layout(matrix(c(2,1), nrow = 2, ncol = 1), widths = 1, heights = c(1, key.size)),  # 1 bottom
 					layout(matrix(c(1,2), nrow = 1, ncol = 2), widths = c(key.size, 1), heights = 1),  # 2 left

man/plot.Rd
@@ -32,7 +32,7 @@
 \method{plot}{sf}(x, y, ..., col = NULL, main, pal = NULL, nbreaks = 10,
   breaks = "pretty", max.plot = if (is.null(n <-
   options("sf_max.plot")[[1]])) 9 else n, key.pos = if (ncol(x) > 2) NULL else
-  4)
+  4, key.size = lcm(2.8))
 
 \method{plot}{sfc_POINT}(x, y, ..., pch = 1, cex = 1, col = 1, bg = 0,
   lwd = 1, lty = 1, type = "p", add = FALSE)
@@ -90,6 +90,8 @@ sf.colors(n = 10, cutoff.tails = c(0.35, 0.2), alpha = 1,
 \item{max.plot}{integer; lower boundary to maximum number of attributes to plot; the default value (9) can be overriden by setting the global option \code{sf_max.plot}, e.g. \code{options(sf_max.plot=2)}}
 
 \item{key.pos}{integer; which side to plot a color key: 1 bottom, 2 left, 3 top, 4 right. Set to \code{NULL} for no key. Currently ignored if multiple columns are plotted.}
+
+\item{key.size}{amount of space reserved for the key (labels)}
 
 \item{pch}{plotting symbol}
 

tests/cast.Rout.save
@@ -1,5 +1,5 @@
 
-R version 3.4.1 (2017-06-30) -- "Single Candle"
+R version 3.4.2 (2017-09-28) -- "Short Summer"
 Copyright (C) 2017 The R Foundation for Statistical Computing
 Platform: x86_64-pc-linux-gnu (64-bit)
 
@@ -115,18 +115,7 @@ MULTILINESTRING ((1 3))
 MULTILINESTRING ((10 13))
 MULTILINESTRING ((11 14))
 MULTILINESTRING ((12 15))
-> (zz = st_cast(yy, "POLYGON"))
-Geometry set for 5 features 
-geometry type:  POLYGON
-dimension:      XY
-bbox:           xmin: 0 ymin: 2 xmax: 12 ymax: 15
-epsg (SRID):    NA
-proj4string:    NA
-POLYGON ((0 2))
-POLYGON ((1 3))
-POLYGON ((10 13))
-POLYGON ((11 14))
-POLYGON ((12 15))
+> #(zz = st_cast(yy, "POLYGON"))
 > 
 > st_cast(mls, "LINESTRING")
 Geometry set for 3 features 
@@ -254,4 +243,4 @@ POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0), (5 5, 5 6, 6 6, 6 5, 5 5))
 > 
 > proc.time()
    user  system elapsed 
-  0.844   0.288   0.827 
+  0.496   0.304   0.480 

tests/gdal_geom.Rout.save
@@ -288,7 +288,7 @@ Error in st_buffer.sfc(sfc, units::set_units(1000, km)) :
 Error : cannot convert km into °
 In addition: Warning message:
 In st_buffer.sfc(sfc, units::set_units(1000, km)) :
-  st_buffer does not correctly buffer longitude/latitude data, dist needs to be in decimal degrees.
+  st_buffer does not correctly buffer longitude/latitude data
 > if (version$os == "linux-gnu") { # why does this break on windows - degree symbol?
 +   print(st_buffer(sfc, units::set_units(0.1, rad)))      # OK: will convert to arc_degrees
 + }
@@ -302,7 +302,7 @@ POLYGON ((12.7295779513082 52, 12.721725763855 ...
 POLYGON ((-24.2704220486918 20, -24.27827423614...
 Warning message:
 In st_buffer.sfc(sfc, units::set_units(0.1, rad)) :
-  st_buffer does not correctly buffer longitude/latitude data, dist needs to be in decimal degrees.
+  st_buffer does not correctly buffer longitude/latitude data
 > x = st_transform(sfc, 3857)
 > st_buffer(x, units::set_units(1000, km)) # success
 Geometry set for 2 features 
@@ -337,4 +337,4 @@ lines[GRID.lines.1]
 > 
 > proc.time()
    user  system elapsed 
-  0.880   0.292   0.876 
+  0.880   0.268   0.854 

tests/plot.Rout.save
@@ -137,15 +137,23 @@ plotting the first 10 out of 14 attributes; use max.plot = 14 to plot all
 > nc[[1]] = NA
 > nc[[10]] = 1
 > plot(nc, pal = rainbow, nbreaks = 3)
-Warning message:
-plotting the first 10 out of 14 attributes; use max.plot = 14 to plot all 
+Warning messages:
+1: plotting the first 10 out of 14 attributes; use max.plot = 14 to plot all 
+2: In min(x, na.rm = na.rm) :
+  no non-missing arguments to min; returning Inf
+3: In max(x, na.rm = na.rm) :
+  no non-missing arguments to max; returning -Inf
 > plot(nc, pal = rainbow, breaks = "jenks", nbreaks = 3)
-Warning message:
-plotting the first 10 out of 14 attributes; use max.plot = 14 to plot all 
+Warning messages:
+1: plotting the first 10 out of 14 attributes; use max.plot = 14 to plot all 
+2: In min(x, na.rm = na.rm) :
+  no non-missing arguments to min; returning Inf
+3: In max(x, na.rm = na.rm) :
+  no non-missing arguments to max; returning -Inf
 > plot(nc, pal = rainbow, breaks = (0:10)/3)
 Warning message:
 plotting the first 10 out of 14 attributes; use max.plot = 14 to plot all 
 > 
 > proc.time()
    user  system elapsed 
-  6.036   0.280   6.010 
+  6.240   0.316   6.236 