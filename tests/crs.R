87f8e5c4dbd185e9f84e226564b64686a07a9a4f
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2018-03-17 18:09:16 +0100

    addresses Fedora 26 issue

--- tests/crs.R
+++ tests/crs.R
@@ -4,9 +4,9 @@ st_crs(999999)
 try(st_crs("error"))
 str = "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.4171,50.3319,465.5524,-0.398957388243134,0.343987817378283,-1.87740163998045,4.0725 +units=m +no_defs"
 st_crs(str)
-st_crs(28992)
-str = st_crs(28992)$proj4string
-st_crs(28992)$units
+st_crs(3857)
+str = st_crs(3857)$proj4string
+st_crs(3857)$units
 st_crs("+proj=longlat +datum=WGS84")
 st_crs(4326)
 st_crs("+proj=laea") # no EPSG
