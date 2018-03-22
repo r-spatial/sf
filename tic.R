5b4d3258dbdb9648da92e0c012b9131d32818272
pat-s <patrick.schratz@gmail.com>
2018-03-23 00:54:42 +0100

    require lwgeom for vignette

--- tic.R
+++ tic.R
@@ -1,11 +1,15 @@
 add_package_checks()
 
+get_stage("script") %>%
+	add_code_step(if (length(find.package("lwgeom", quiet = TRUE)) == 0) install.packages("lwgeom")) # for vignette checking
+
 if (Sys.getenv("id_rsa") != "") {
   # pkgdown documentation can be built optionally. Other example criteria:
   # - `inherits(ci(), "TravisCI")`: Only for Travis CI
   # - `ci()$is_tag()`: Only for tags, not for branches
   # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
   # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
+
   get_stage("before_deploy") %>%
     add_step(step_setup_ssh())
 
