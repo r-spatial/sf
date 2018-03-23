c3f767f83c03f04fa94b570dc3ba9c20b785b64c
pat-s <patrick.schratz@gmail.com>
2018-03-23 16:30:05 +0100

    install dev_deps?

--- tic.R
+++ tic.R
@@ -1,7 +1,4 @@
-#add_package_checks(args = c("--no-vignettes", "--no-build-vignettes"))
-
-get_stage("script") %>%
-	add_step(step_rcmdcheck(args = c("--no-vignettes", "--no-build-vignettes")))
+add_package_checks()
 
 if (Sys.getenv("id_rsa") != "") {
   # pkgdown documentation can be built optionally. Other example criteria:
