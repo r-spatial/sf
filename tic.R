85517a3d23a5f7761ab606b0e783e7f716b4c55c
pat-s <patrick.schratz@gmail.com>
2018-03-23 22:46:07 +0100

    old after_success

--- tic.R
+++ tic.R
@@ -4,12 +4,6 @@ get_stage("install") %>%
 	# install lwgeom with its own library since linking again postgis source install fails sometimes
 	add_code_step(install.packages("lwgeom", configure.args="--without-liblwgeom"))
 
-get_stage("after_success") %>%
-	add_code_step(system("dropdb postgis")) %>%
-	add_code_step(system("createdb postgis")) %>%
-	add_code_step(system("psql -d postgis -c")) # CREATE EXTENSION postgis
-    add_code_step(system("psql -d postgis -c")) # GRANT CREATE ON DATABASE postgis TO travis
-
 ###
 # deploy pkgdowm site
 ###
