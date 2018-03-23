f4a9e8b20dccc95a02d28fcff6cbbd9f7c355c17
pat-s <patrick.schratz@gmail.com>
2018-03-23 01:13:11 +0100

    skip vignettes

--- tic.R
+++ tic.R
@@ -1,7 +1,7 @@
 add_package_checks()
 
 get_stage("script") %>%
-	add_code_step(if (length(find.package("lwgeom", quiet = TRUE)) == 0) install.packages("lwgeom")) # for vignette checking
+	add_step(step_rcmdcheck(args = c("--no-vignettes", "--no-build-vignettes")))
 
 if (Sys.getenv("id_rsa") != "") {
   # pkgdown documentation can be built optionally. Other example criteria:
