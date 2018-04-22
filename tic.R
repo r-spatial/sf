8b3fb6c4d790752f604f9319cd097d51961dded9
pat-s <patrick.schratz@gmail.com>
2018-04-22 21:31:06 +0200

    deploy to gh-pages again

--- tic.R
+++ tic.R
@@ -14,5 +14,5 @@ if (Sys.getenv("id_rsa") != "") {
 
   get_stage("deploy") %>%
     add_step(step_build_pkgdown()) %>%
-    add_step(step_push_deploy())
+    add_step(step_push_deploy(path = "docs", branch = "gh-pages", orphan = TRUE))
 }
