3b37431718f7a23553179700b9e918661937560a
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2018-06-08 08:09:41 +0200

    coverage

--- src/gdal.cpp
+++ src/gdal.cpp
@@ -168,13 +168,10 @@ std::vector<OGRGeometry *> ogr_from_sfc(Rcpp::List sfc, OGRSpatialReference **sr
 		Rcpp::RawVector r = wkblst[i];
 		OGRErr err = OGRGeometryFactory::createFromWkb(&(r[0]), local_srs, &(g[i]), 
 			r.length(), wkbVariantIso);
-		if (err != 0) {
-			if (g[i] != NULL)
-				 OGRGeometryFactory::destroyGeometry(g[i]);
-			g[i] = NULL;
-			if (local_srs != NULL)      // #nocov
-				local_srs->Release();   // #nocov
-			handle_error(err);          // #nocov
+		if (err != OGRERR_NONE) {
+			if (local_srs != NULL)      // #nocov start
+				local_srs->Release();
+			handle_error(err);          // #nocov end
 		}
 	}
 	if (sref != NULL)
