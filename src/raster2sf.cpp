24fc10daf6f5d92419a49f669d500d38b16fbf68
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2018-07-12 22:24:00 +0200

    fix xy2sf

--- src/raster2sf.cpp
+++ src/raster2sf.cpp
@@ -30,19 +30,20 @@ List CPL_xy2sfc(NumericMatrix cc, IntegerVector dim, bool to_points, IntegerVect
 		List ret(which.length());
 		for (int i = 0; i < which.length(); i++) {
 			int ix = which[i] - 1; // from R, 1-based
-			size_t y = ix / dim[1]; // row index
+			size_t y = ix / (dim[0] - 1); // row index
 			size_t x = ix % (dim[0] - 1); // col index
+			// Rcpp::Rcout << "x is:" << x << " y is:" << y << std::endl;
 			NumericMatrix points(5, 2); // the four corners
-			points(0,0) = cc(y * dim[0] + x,           0); // top left
-			points(0,1) = cc(y * dim[0] + x,           1); // top left
-			points(1,0) = cc(y * dim[0] + x + 1,       0); // top right
-			points(1,1) = cc(y * dim[0] + x + 1,       1); // top right
-			points(2,0) = cc((y + 1) * dim[0] + x + 1, 0); // bottom right
-			points(2,1) = cc((y + 1) * dim[0] + x + 1, 1); // bottom right
-			points(3,0) = cc((y + 1) * dim[0] + x,     0); // bottom left
-			points(3,1) = cc((y + 1) * dim[0] + x,     1); // bottom left
-			points(4,0) = cc(y * dim[0] + x,           0); // top left
-			points(4,1) = cc(y * dim[0] + x,           1); // top left
+			points(0,0) = cc(y * (dim[0]) + x    ,   0); // top left
+			points(0,1) = cc(y * (dim[0]) + x    ,   1); // top left
+			points(1,0) = cc(y * (dim[0]) + x + 1,   0); // top right
+			points(1,1) = cc(y * (dim[0]) + x + 1,   1); // top right
+			points(2,0) = cc((y + 1) * (dim[0]) + x + 1, 0); // bottom right
+			points(2,1) = cc((y + 1) * (dim[0]) + x + 1, 1); // bottom right
+			points(3,0) = cc((y + 1) * (dim[0]) + x    , 0); // bottom left
+			points(3,1) = cc((y + 1) * (dim[0]) + x    , 1); // bottom left
+			points(4,0) = cc(y * (dim[0]) + x    ,       0); // top left
+			points(4,1) = cc(y * (dim[0]) + x    ,       1); // top left
 			List polygon(1);
 			polygon.attr("class") = CharacterVector::create("XY", "POLYGON", "sfg");
 			polygon(0) = points;
