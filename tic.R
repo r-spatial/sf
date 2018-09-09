4e58778fc0c98da658acc1a386d822e4db970912
pat-s <patrick.schratz@gmail.com>
2018-09-09 15:30:39 +0200

    clean

--- tic.R
+++ tic.R
@@ -2,8 +2,7 @@ add_package_checks()
 
 get_stage("install") %>%
 	# install lwgeom with its own library since linking again postgis source install fails sometimes
-	add_code_step(install.packages("lwgeom", configure.args="--without-liblwgeom"))# %>%
-	#add_code_step(devtools::install_github("pat-s/rcmdcheck@catch-test-errors"))
+	add_code_step(install.packages("lwgeom", configure.args="--without-liblwgeom"))
 
 ###
 # deploy pkgdowm site
