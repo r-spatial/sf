52f59a9d8d2ca5aaf228951a8ba50e5c8511f44f
Robin Lovelace <rob00x@gmail.com>
2018-11-13 17:35:47 +0000

    Add examples showing exact

--- R/sample.R
+++ R/sample.R
@@ -25,8 +25,11 @@
 #'   p2 = st_transform(p, st_crs("+proj=ortho +lat_0=30 +lon_0=45"))
 #'   plot(p2, add = TRUE)
 #' }
-#' x = st_sfc(st_polygon(list(rbind(c(0,0),c(90,0),c(90,90),c(0,90),c(0,0))))) # NOT long/lat:
+#' x = st_sfc(st_polygon(list(rbind(c(0,0),c(90,0),c(90,10),c(0,90),c(0,0))))) # NOT long/lat:
 #' plot(x)
+#' p_exact = st_sample(x, 1000, exact = TRUE)
+#' p_not_exact = st_sample(x, 1000, exact = FALSE)
+#' length(p_exact); length(p_not_exact)
 #' plot(st_sample(x, 1000), add = TRUE)
 #' x = st_sfc(st_polygon(list(rbind(c(-180,-90),c(180,-90),c(180,90),c(-180,90),c(-180,-90)))),
 #'	 crs=st_crs(4326))
@@ -39,15 +42,22 @@
 #' plot(sfc)
 #' h = st_sample(sfc, 100, type = "hexagonal")
 #' h1 = st_sample(sfc, 100, type = "hexagonal")
-#' c(length(h), length(h1)) # approximate!
 #' plot(h, add = TRUE)
 #' plot(h1, col = 'red', add = TRUE)
+#' c(length(h), length(h1)) # approximate!
+#' h_e = st_sample(sfc, 100, type = "hexagonal", exact = TRUE)
+#' h1_e = st_sample_exact(sfc, 100, type = "hexagonal")
+#' c(length(h_e), length(h1_e)) # exact
+#' plot(sfc)
+#' plot(h_e, add = TRUE) # may not be evenly distributed
+#' plot(h1_e, col = 'red', add = TRUE)
 #' pt = st_multipoint(matrix(1:20,,2))
 #' ls = st_sfc(st_linestring(rbind(c(0,0),c(0,1))),
 #'  st_linestring(rbind(c(0,0),c(.1,0))),
 #'  st_linestring(rbind(c(0,1),c(.1,1))),
 #'  st_linestring(rbind(c(2,2),c(2,2.00001))))
 #' st_sample(ls, 80)
+#'
 #' @export
 st_sample = function(x, size, ..., type = "random", exact = FALSE) {
 	x = st_geometry(x)
@@ -57,14 +67,14 @@ st_sample = function(x, size, ..., type = "random", exact = FALSE) {
 		res = st_set_crs(do.call(c, ret), st_crs(x))
 	} else {
 		res = switch(max(st_dimension(x)) + 1,
-			st_multipoints_sample(do.call(c, x), size, ..., type = type),
-			st_ll_sample(st_cast(x, "LINESTRING"), size, ..., type = type),
-			st_poly_sample(x, size, ..., type = type))
+					 st_multipoints_sample(do.call(c, x), size, ..., type = type),
+					 st_ll_sample(st_cast(x, "LINESTRING"), size, ..., type = type),
+					 st_poly_sample(x, size, ..., type = type))
 	}
 	if (exact) {
 		diff = size - length(res)
 		if(diff > 0) { # too few points
-			res_additional = st_sample_exact(x = x, size = diff)
+			res_additional = st_sample_exact(x, size, ..., type = type)
 			res = c(res, res_additional)
 		} else if (diff < 0) { # too many points
 			res = res[1:size]
@@ -74,7 +84,7 @@ st_sample = function(x, size, ..., type = "random", exact = FALSE) {
 }
 
 st_poly_sample = function(x, size, ..., type = "random",
-		offset = st_sample(st_as_sfc(st_bbox(x)), 1)[[1]]) {
+						  offset = st_sample(st_as_sfc(st_bbox(x)), 1)[[1]]) {
 
 	a0 = as.numeric(st_area(st_make_grid(x, n = c(1,1))))
 	a1 = as.numeric(sum(st_area(x)))
@@ -93,7 +103,7 @@ st_poly_sample = function(x, size, ..., type = "random",
 	} else if (type == "regular") {
 		dx = as.numeric(sqrt(a0 / size))
 		offset = c((offset[1] - bb["xmin"]) %% dx,
-			(offset[2] - bb["ymin"]) %% dx) + bb[c("xmin", "ymin")]
+				   (offset[2] - bb["ymin"]) %% dx) + bb[c("xmin", "ymin")]
 		n = c(round((bb["xmax"] - offset[1])/dx), round((bb["ymax"] - offset[2])/dx))
 		st_make_grid(x, cellsize = c(dx, dx), offset = offset, n = n, what = "corners")
 	} else if (type == "random") {
@@ -147,7 +157,7 @@ st_ll_sample = function (x, size, ..., type = "random", offset = runif(1)) {
 ## - has x spacing dx: the shortest distance between x coordinates with identical y coordinate
 ## - selects geometries intersecting with obj
 hex_grid = function(obj, pt = bb[c("xmin", "ymin")],
-		dx = diff(st_bbox(obj)[c("xmin", "xmax")])/10.1, points = TRUE, clip = NA) {
+					dx = diff(st_bbox(obj)[c("xmin", "xmax")])/10.1, points = TRUE, clip = NA) {
 
 	bb = st_bbox(obj)
 	dy = sqrt(3) * dx / 2
@@ -161,7 +171,7 @@ hex_grid = function(obj, pt = bb[c("xmin", "ymin")],
 	x <- rep(c(x, x + dx / 2), length.out = length(y))
 	ret = if (points) {
 		xy = cbind(x, y)[x >= xlim[1] & x <= xlim[2] & y >= ylim[1] & y <= ylim[2], ]
-    	st_sfc(lapply(seq_len(nrow(xy)), function(i) st_point(xy[i,])), crs = st_crs(bb))
+		st_sfc(lapply(seq_len(nrow(xy)), function(i) st_point(xy[i,])), crs = st_crs(bb))
 	} else {
 		dy = dx / sqrt(3)
 		dx2 = dx / 2
@@ -181,8 +191,8 @@ hex_grid = function(obj, pt = bb[c("xmin", "ymin")],
 	ret[sel]
 }
 
-st_sample_exact <- function(x, size, ..., type = "random") {
-	random_pt <- st_sample(x, size, ..., type, exact = FALSE)
+st_sample_exact <- function(x, size, ..., type) {
+	random_pt <- st_sample(x = x, size = size, ..., type = type, exact = FALSE)
 	while (length(random_pt) < size) {
 		diff <- size - length(random_pt)
 		random_pt_new <- st_sample(x, size, ..., type, exact = FALSE)
