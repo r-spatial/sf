6a93c460186a9704965732a968830dbef6af7486
Hiroaki Yutani <yutani.ini@gmail.com>
2019-01-25 16:11:23 +0900

    Describe some details about nc dataset

--- R/datasets.R
+++ R/datasets.R
@@ -1,4 +1,11 @@
-#' North Carolina counties SIDS dataset
+#' North Carolina SIDS data
+#'
+#' Sudden Infant Death Syndrome (SIDS) sample data for North Carolina counties,
+#' two time periods (1974-78 and 1979-84). The details of the columns can be
+#' found on the seealso URL, spdep package's vignette. Please note that,
+#' though this is basically the same as \code{nc.sids} dataset in spData
+#' package, \code{nc} only contains a subset of variables. The differences are
+#' also discussed on the vignette.
 #'
 #' @name nc
 #' @docType data
