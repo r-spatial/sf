c020f72f7bef580d8f1a7647d1e6863d1ad15cfe
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2019-03-13 17:04:23 +0100

    Use PROJ 6 api if present; #988
    this seems to do the job:
    * has a --with-proj-api=no default, and --with-proj-api=yes option that only works if PROJ is 6
    * still has all the proj_info kludge, now in a separte file
    * does the conditioning in ./configure

--- src/proj.cpp
+++ src/proj.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-#ifdef PROJH
-#include <proj.h>
 #include "Rcpp.h"
 
+#if defined(HAVE_PROJ_H) && !defined(ACCEPT_USE_OF_DEPRECATED_PROJ_API_H) // new api
+#include <proj.h>
 
 std::string CPL_proj_version(bool b = false) {
 
@@ -24,10 +24,8 @@ Rcpp::List CPL_proj_is_valid(std::string proj4string) {
 		out(0) = Rcpp::LogicalVector::create(true);
 		PJ_PROJ_INFO pi;
 		pi = proj_pj_info(P);
-		const char *def = pi.description; // or .definition?
-		out(1) = Rcpp::CharacterVector::create(def);
+		out(1) = Rcpp::CharacterVector::create(pi.description);
 		proj_destroy(P);
-		// free(def); // ???
 	}
 	return out;
 }
@@ -35,7 +33,8 @@ Rcpp::List CPL_proj_is_valid(std::string proj4string) {
 bool CPL_have_datum_files(SEXP foo) {
 
 	// TODO:
-	// create a PJ with conus, check success, if yes destroy, return
+	// create a PJ with e.g. conus, check success, if yes destroy, return success
+	Rcpp::warning("CPL_have_datum not yet implemented for PROJ6 proj.h interface");
 	return true;
 }
 
@@ -103,14 +102,13 @@ Rcpp::NumericMatrix CPL_proj_direct(Rcpp::CharacterVector from_to, Rcpp::Numeric
 	return out;
 }
 
-#else // proj_api.h:
 
+#else // if defined(HAVE_PROJ_H) && !defined(ACCEPT_USE_OF_DEPRECATED_PROJ_API_H) // new api
 #include <proj_api.h>
 
 #if PJ_VERSION >= 600
 # define PROJ6 1
 #endif
-
 
 #if PJ_VERSION == 480
 extern "C" {
@@ -230,178 +228,5 @@ Rcpp::NumericMatrix CPL_proj_direct(Rcpp::CharacterVector from_to, Rcpp::Numeric
 		warning("one or more projected point(s) not finite"); // #nocov
 	return out;
 }
+#endif // defined() etc
 
-#ifndef PROJ6
-
-extern "C" {
-// modified from: rgdal/pkg/src/projectit.cpp
-
-// copied necessary parts from projects.h; full header conflicts with R headers;
-// should these things ever change in the proj API, then we're in trouble.
-struct PJconsts;
-struct PJ_LIST {
-	char	*id;		/* projection keyword */
-	struct PJconsts	*(*proj)(struct PJconsts*);/* projection entry point */
-	char 	* const *descr;	/* description text */
-};
-struct PJ_LIST *pj_get_list_ref( void );
-struct PJ_ELLPS {
-	char	*id;	/* ellipse keyword name */
-	char	*major;	/* a= value */
-	char	*ell;	/* elliptical parameter */
-	char	*name;	/* comments */
-};
-struct PJ_ELLPS *pj_get_ellps_ref( void );
-struct PJ_DATUMS {
-	char	*id;	 /* datum keyword */
-	char	*defn;	 /* ie. "to_wgs84=..." */
-	char	*ellipse_id; /* ie from ellipse table */
-	char	*comments; /* EPSG code, etc */
-};
-struct PJ_DATUMS *pj_get_datums_ref( void ); 
-struct PJ_UNITS {
-	char	*id;		/* units keyword */
-	char	*to_meter;	/* multiply by value to get meters */
-	char	*name;		/* comments */
-#if PJ_VERSION >= 500
-	double   factor;       /* to_meter factor in actual numbers */
-#endif
-};
-struct PJ_UNITS *pj_get_units_ref( void );
-
-} // extern "C"
-
-Rcpp::List CPL_proj_info(int type) {
-
-	Rcpp::List ret;
-	switch (type) {
-		case 0: {
-			Rcpp::List ans(2);
-			ans.attr("names") = Rcpp::CharacterVector::create("name", "description");
-			int n = 0;
-			struct PJ_LIST *lp;
-			for (lp = pj_get_list_ref() ; lp->id ; ++lp)
-				n++;
-			Rcpp::CharacterVector cv0(n);
-			Rcpp::CharacterVector cv1(n);
-			n = 0;
-			for (lp = pj_get_list_ref() ; lp->id ; ++lp) {
-				cv0(n) = lp->id;
-				cv1(n) = *lp->descr;
-				n++;
-			}
-			ans(0) = cv0;
-			ans(1) = cv1;
-			ret = ans;
-		} break;
-		case 1: {
-			Rcpp::List ans(4);
-			ans.attr("names") = Rcpp::CharacterVector::create("name", 
-				"major", "ell", "description");
-			int n = 0;
-			struct PJ_ELLPS *le;
-			for (le = pj_get_ellps_ref(); le->id ; ++le) 
-				n++;
-			Rcpp::CharacterVector ans0(n);
-			Rcpp::CharacterVector ans1(n);
-			Rcpp::CharacterVector ans2(n);
-			Rcpp::CharacterVector ans3(n);
-			n = 0;
-			for (le = pj_get_ellps_ref(); le->id ; ++le) {
-				ans0(n) = le->id;
-				ans1(n) = le->major;
-				ans2(n) = le->ell;
-				ans3(n) = le->name;
-				n++;
-			}
-			ans(0) = ans0;
-			ans(1) = ans1;
-			ans(2) = ans2;
-			ans(3) = ans3;
-			ret = ans;
-		} break;
-		case 2: {
-			Rcpp::List ans(4);
-			ans.attr("names") = Rcpp::CharacterVector::create("name", "ellipse",
-				"definition", "description");
-			int n = 0;
-			struct PJ_DATUMS *ld;
-			for (ld = pj_get_datums_ref(); ld->id ; ++ld) 
-				n++;
-			Rcpp::CharacterVector ans0(n);
-			Rcpp::CharacterVector ans1(n);
-			Rcpp::CharacterVector ans2(n);
-			Rcpp::CharacterVector ans3(n);
-			n = 0;
-			for (ld = pj_get_datums_ref(); ld->id ; ++ld) {
-				ans0(n) = ld->id;
-				ans1(n) = ld->ellipse_id;
-				ans2(n) = ld->defn;
-				ans3(n) = ld->comments;
-				n++;
-			}
-			ans(0) = ans0;
-			ans(1) = ans1;
-			ans(2) = ans2;
-			ans(3) = ans3;
-			ret = ans;
-		} break;
-		case 3: {
-#if PJ_VERSION >= 500
-			Rcpp::List ans(4);
-			ans.attr("names") = Rcpp::CharacterVector::create("id", "to_meter",
-				"name", "factor");
-			int n = 0;
-			struct PJ_UNITS *ld;
-			for (ld = pj_get_units_ref(); ld->id ; ++ld) 
-				n++;
-			Rcpp::CharacterVector ans0(n);
-			Rcpp::CharacterVector ans1(n);
-			Rcpp::CharacterVector ans2(n);
-			Rcpp::NumericVector ans3(n);
-			n = 0;
-			for (ld = pj_get_units_ref(); ld->id ; ++ld) {
-				ans0(n) = ld->id;
-				ans1(n) = ld->to_meter;
-				ans2(n) = ld->name;
-				ans3(n) = ld->factor;
-				n++;
-			}
-			ans(0) = ans0;
-			ans(1) = ans1;
-			ans(2) = ans2;
-			ans(3) = ans3;
-			ret = ans;
-#else
-			Rcpp::List ans(3);
-			ans.attr("names") = Rcpp::CharacterVector::create("id", "to_meter",
-				"name");
-			int n = 0;
-			struct PJ_UNITS *ld;
-			for (ld = pj_get_units_ref(); ld->id ; ++ld) 
-				n++;
-			Rcpp::CharacterVector ans0(n);
-			Rcpp::CharacterVector ans1(n);
-			Rcpp::CharacterVector ans2(n);
-			n = 0;
-			for (ld = pj_get_units_ref(); ld->id ; ++ld) {
-				ans0(n) = ld->id;
-				ans1(n) = ld->to_meter;
-				ans2(n) = ld->name;
-				n++;
-			}
-			ans(0) = ans0;
-			ans(1) = ans1;
-			ans(2) = ans2;
-			ret = ans;
-#endif
-		} break;
-		default:
-			Rcpp::stop("unknown type"); // #nocov
-		break;
-	}
-	return ret;
-}
-#endif
-
-#endif // PROJH
