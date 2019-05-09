56b5a5722f00fcef3a7fff29341228061517c70d
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2019-05-09 10:01:56 +0200

    tidy

--- R/plot.R
+++ R/plot.R
@@ -104,7 +104,8 @@ plot.sf <- function(x, y, ..., main, pal = NULL, nbreaks = 10, breaks = "pretty"
 			if (is.character(breaks)) { # compute breaks from values:
 				n.unq = length(unique(na.omit(values)))
 				breaks = if (! all(is.na(values)) && n.unq > 1)
-						classInt::classIntervals(na.omit(values), min(nbreaks, n.unq), breaks, warnSmallN = FALSE)$brks
+						classInt::classIntervals(na.omit(values), min(nbreaks, n.unq), 
+							breaks, warnSmallN = FALSE)$brks
 					else
 						range(values, na.rm = TRUE) # lowest and highest!
 				nbreaks = length(breaks) - 1
@@ -178,7 +179,8 @@ plot.sf <- function(x, y, ..., main, pal = NULL, nbreaks = 10, breaks = "pretty"
 						if (is.character(breaks)) { # compute breaks from values:
 							n.unq = length(unique(na.omit(values)))
 							breaks = if (! all(is.na(values)) && n.unq > 1)
-								classInt::classIntervals(na.omit(values), min(nbreaks, n.unq), breaks, warnSmallN = FALSE)$brks
+								classInt::classIntervals(na.omit(values), min(nbreaks, n.unq),
+									breaks, warnSmallN = FALSE)$brks
 							else
 								range(values, na.rm = TRUE) # lowest and highest!
 						} 
