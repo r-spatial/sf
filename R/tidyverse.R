05f12429b2b81fc5fd456080499508ed0dab1bcd
Lionel Henry <lionel.hry@gmail.com>
2019-11-20 10:04:09 +0100

    Move vctrs methods to their own file

--- R/tidyverse.R
+++ R/tidyverse.R
@@ -377,67 +377,6 @@ pillar_shaft.sfc <- function(x, ...) {
 	pillar::new_pillar_shaft_simple(out, align = "right", min_width = 25)
 }
 
-# This returns the memory representation of `sfc` vectors. At the same
-# time, this declares `sfc` lists as vectors which is necessary
-# because vctrs generally treats S3 lists as scalars.
-vec_proxy.sfc <- function(x, ...) {
-	x
-}
-# This restores `sfc` attributes after manipulation of the proxy
-# (e.g. slicing or combination)
-vec_restore.sfc <- function(x, to, ...) {
-	# Ensure restoration of `n_empty` by `st_sfc()`
-	attr(x, "n_empty") = NULL
-
-	st_sfc(x, crs = st_crs(to), precision = st_precision(to))
-}
-
-#' vctrs methods for sf objects
-#' @name vctrs
-#' @export
-#' @inheritParams vctrs::vec_ptype2
-vec_ptype2.sfc <- function(x, y, ...) {
-	UseMethod("vec_ptype2.sfc", y)
-}
-#' @name vctrs
-#' @export
-vec_ptype2.sfc.default <- function(x, y, ..., x_arg = "x", y_arg = "y") {
-	vctrs::vec_default_ptype2(x, y, x_arg = x_arg, y_arg = y_arg)
-}
-#' @name vctrs
-#' @export
-vec_ptype2.sfc.sfc <- function(x, y, ...) {
-	ucls = unique(c(class(x)[1], class(y)[1]))
-	cls =
-		if (length(ucls) > 1) # a mix:
-			c("sfc_GEOMETRY", "sfc")
-		else
-			c(ucls, "sfc")
-	structure(st_sfc(), class = cls)
-}
-
-
-# minimal vec_cast implementation: https://github.com/r-spatial/sf/issues/1068
-#' @name vctrs
-#' @export
-#' @inheritParams vctrs::vec_cast
-#' @param x_arg,y_arg Argument names for \code{x} and \code{y}.
-vec_cast.sfc <- function(x, to, ...) UseMethod("vec_cast.sfc") # nocov
-
-#' @name vctrs
-#' @export
-vec_cast.sfc.sfc <- function(x, to, ...) {
-	st_cast(x, gsub("sfc_", "", class(to)[1])) # nocov
-}
-
-#' @name vctrs
-#' @export
-vec_cast.sfc.default <- function(x, to, ...) {
-	if (!requireNamespace("vctrs", quietly = TRUE)) # nocov start
-		stop("vctrs not available: install first?")
-	vctrs::vec_default_cast(x, to) # nocov end
-}
-
 register_all_s3_methods = function() {
 	register_s3_method("dplyr", "anti_join", "sf")
 	register_s3_method("dplyr", "arrange", "sf")
@@ -470,10 +409,7 @@ register_all_s3_methods = function() {
 	register_s3_method("pillar", "obj_sum", "sfc")
 	register_s3_method("pillar", "type_sum", "sfc")
 	register_s3_method("pillar", "pillar_shaft", "sfc")
-	register_s3_method("vctrs", "vec_proxy", "sfc")
-	register_s3_method("vctrs", "vec_restore", "sfc")
-	register_s3_method("vctrs", "vec_ptype2", "sfc")
-	register_s3_method("vctrs", "vec_cast", "sfc")
+	register_vctrs_methods()
 }
 
 # from: https://github.com/tidyverse/hms/blob/master/R/zzz.R
