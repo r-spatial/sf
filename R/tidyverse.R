0a1ed37fa36a0139196966f4efb9458dea2270e3
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2019-11-22 20:27:23 +0100

    attempt at #931

--- R/tidyverse.R
+++ R/tidyverse.R
@@ -358,7 +358,7 @@ type_sum.sfc <- function(x, ...) {
 		#paste0(cls, " [", enc2utf8(as.character(units(st_crs(x, parameters = TRUE)$ud_unit))), "]")
 		u = as.character(units(st_crs(x, parameters = TRUE)$ud_unit))
 		Encoding(u) = "UTF-8"
-		paste0(cls, " [", u, "]")
+		paste0(cls, " [", enc2native(u), "]")
 	}
 }
 
