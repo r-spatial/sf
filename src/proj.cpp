cca1e134f139b42f3a83472686a32c1a51e5f4fd
dankelley <kelley.dan@gmail.com>
2019-12-29 10:01:47 -0400

    return Inf instead of NA for unprojectables

--- src/proj.cpp
+++ src/proj.cpp
@@ -247,14 +247,13 @@ Rcpp::NumericMatrix CPL_proj_direct(Rcpp::CharacterVector from_to, Rcpp::Numeric
 //  		 Rcout << xx[i] << " " << yy[i] << std::endl;
         if (keep[0] == 1) {
             // use proj_trans() on individual points, making unprojectable points be NA
-            // FIXME: not tested, since author has no access to the old proj API.
             double thisx, thisy;
             for (int i = 0; i < pts.nrow(); i++) {
                 thisx = xx[i];
                 thisy = yy[i];
                 if (pj_transform(fromPJ, toPJ, 1, 0, &thisx, &thisy, NULL) != 0) {
-                    xx[i] = NA_REAL;
-                    yy[i] = NA_REAL;
+                    xx[i] = R_PosInf;
+                    yy[i] = R_PosInf;
                 } else {
                     xx[i] = thisx;
                     yy[i] = thisy;
