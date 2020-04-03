a299ba94c2c8a54db197303b9242f9b2e914580e
dcooley <david.cooley141@gmail.com>
2020-04-03 12:58:59 -0700

    addressing r-spatial/sf#1332

--- src/zm_range.h
+++ src/zm_range.h
@@ -1,4 +1,12 @@
-Rcpp::NumericVector CPL_get_zm_range(Rcpp::List sf, int depth);
+//Rcpp::NumericVector CPL_get_zm_range(Rcpp::List sf, int depth);
+
+int get_m_position(Rcpp::NumericVector& pt);
+
+int get_m_position(Rcpp::NumericMatrix& nm);
+
+int get_z_position(Rcpp::NumericVector& pt);
+
+int get_z_position(Rcpp::NumericMatrix& nm);
 
 Rcpp::NumericVector CPL_get_z_range(Rcpp::List sf, int depth);
 
