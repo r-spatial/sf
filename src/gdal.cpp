2d2bac6909f192175aa3aece42dba17cd1270323
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2020-04-14 18:20:21 +0200

    simplify

--- src/gdal.cpp
+++ src/gdal.cpp
@@ -134,22 +134,19 @@ Rcpp::CharacterVector wkt_from_spatial_reference(const OGRSpatialReference *srs)
 	return out;
 }
 
-Rcpp::CharacterVector CPL_wkt_from_user_input(Rcpp::CharacterVector input) {
-	OGRSpatialReference *srs = new OGRSpatialReference;
-	srs = handle_axis_order(srs);
-	handle_error(srs->SetFromUserInput((const char *) input[0]));
-	Rcpp::CharacterVector out = wkt_from_spatial_reference(srs);
-	delete srs;
-	return(out);
-}
-
 Rcpp::List fix_old_style(Rcpp::List crs) {
 	Rcpp::CharacterVector n = crs.attr("names");
 	if (n[0] == "epsg") { // create new: // #nocov start
 		Rcpp::List ret(2);
 		Rcpp::CharacterVector proj4string = crs[1];
-		ret[0] = proj4string[0];
-		ret[1] = CPL_wkt_from_user_input(proj4string);
+		ret[0] = proj4string[0]; // $input
+
+		OGRSpatialReference *srs = new OGRSpatialReference;
+		srs = handle_axis_order(srs);
+		handle_error(srs->SetFromUserInput((const char *) proj4string[0]));
+		ret[1] = wkt_from_spatial_reference(srs); // $wkt
+		delete srs;
+
 		Rcpp::CharacterVector names(2);
 		names(0) = "input";
 		names(1) = "wkt";
