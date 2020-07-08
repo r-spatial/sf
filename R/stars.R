a40f3adf4ad3f286f14021b3135cfdbed5ebe883
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2020-07-08 13:28:29 +0200

    fixes #1389

--- R/stars.R
+++ R/stars.R
@@ -262,7 +262,7 @@ gdal_polygonize = function(x, mask = NULL, file = tempfile(), driver = "GTiff", 
 		else
 			character(0)
 
-	pol = CPL_polygonize(file, mask_name, "GTiff", "Memory", "foo", options, 0, contour_options, use_contours, use_integer)
+	pol = CPL_polygonize(file, mask_name, "GTiff", "GPKG", "foo", options, 0, contour_options, use_contours, use_integer)
 	out = process_cpl_read_ogr(pol, quiet = TRUE)
 	names(out)[1] = names(x)[1]
 	if (use_contours) {
