f8e6d27f3deeab543d57b1334b548dc62a6190f2
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2020-09-17 17:47:58 +0200

    fixes #1489

--- R/make_grid.R
+++ R/make_grid.R
@@ -157,13 +157,8 @@ make_hex_grid = function(obj, pt, dx, what, flat_topped = TRUE) {
 				st_polygon(list(xy[i_from_x(m),2:1]))
 			}
 
-	ret = if (what == "points")
-			st_sfc(lapply(seq_len(nrow(centers)), function(i) mk_point(centers[i,])), crs = st_crs(bb))
-		else # points:
-			st_sfc(lapply(seq_len(nrow(centers)), function(i) mk_pol(centers[i,])), crs = st_crs(bb))
-
-	if (what == "points" || min(st_dimension(obj)) < 2)
-		ret[obj]
-	else 
-		ret[lengths(st_relate(ret, obj, "2********")) > 0] # part or total overlap
+	if (what == "centers")
+		st_sfc(lapply(seq_len(nrow(centers)), function(i) mk_point(centers[i,])), crs = st_crs(bb))
+	else # points:
+		st_sfc(lapply(seq_len(nrow(centers)), function(i) mk_pol(centers[i,])), crs = st_crs(bb))
 }
