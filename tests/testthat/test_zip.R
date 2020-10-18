32fcc96cc0f59c10aa6d6e9a8f2e3568f539957d
Arthur Gailes <agailes1@gmail.com>
2020-10-18 10:08:20 -0700

    add zip test example

---
+++ tests/testthat/test_zip.R
@@ -0,0 +1,8 @@
+context("sf: read tests")
+
+# note that the directory here is relative
+test_that("we can read a zipped shapefile using st_read", {
+	nc <- st_read('inst/zip/nc.zip', "nc", crs = 4267, quiet = TRUE)
+	expect_identical(class(nc), c("sf", "data.frame"))
+	expect_equal(dim(nc), c(100, 15))
+})
