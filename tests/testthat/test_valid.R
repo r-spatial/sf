aadc777e85ec251a3371a7ac12aef1b3845cd6d0
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2021-08-23 22:54:19 +0200

    add test for #1761, from #1760

---
+++ tests/testthat/test_valid.R
@@ -0,0 +1,9 @@
+context("st_is_valid")
+
+# https://github.com/r-spatial/sf/issues/1760
+test_that("st_is_valid", {
+  p1 <- st_as_sfc("POLYGON((0 0, 0 10, 10 10, 10 0, 0 0))")
+  p2 <- st_as_sfc("POLYGON((0 0, 0 10, 10 0))")
+  set1 <- c(p1, p2)
+  expect_identical(st_is_valid(set1), c(TRUE, NA))
+})
