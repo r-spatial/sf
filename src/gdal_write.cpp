1eb9670ed0895cfb3deaf472056fc291490a15ad
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2022-03-04 15:01:03 +0100

    typo

--- src/gdal_write.cpp
+++ src/gdal_write.cpp
@@ -446,7 +446,7 @@ int CPL_delete_ogr(Rcpp::CharacterVector dsn, Rcpp::CharacterVector layer,
 							driver[0] << "'" << std::endl;
 					}
 				}
-				if (err != OGRERR_NONE);
+				if (err != OGRERR_NONE)
 					Rcpp::Rcout << "Deleting layer `" << layer[i] << "' failed" << std::endl;
 			}
 		}
