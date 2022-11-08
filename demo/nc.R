4723db54ddbe845b48e686cd14de65631bc199d6
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2022-11-08 08:57:30 +0100

    Revert "Extra flags in fedora/gcc etc checks"

--- demo/nc.R
+++ demo/nc.R
@@ -20,4 +20,3 @@ agr = c(AREA = "aggregate", PERIMETER = "aggregate", CNTY_ = "identity",
 		BIR79 = "aggregate", SID79 = "aggregate", NWBIR79  = "aggregate")
 
 nc = st_read(datasource, agr = agr, quiet = TRUE)
-
