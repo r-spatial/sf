bb4e140cb3bf6c5ecd1ac952bd349a68ddd98028
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2023-06-05 22:43:09 +0200

    address https://github.com/r-spatial/stars/issues/632

--- R/stars.R
+++ R/stars.R
@@ -192,10 +192,14 @@ st_as_sfc.dimensions = function(x, ..., as_points = NA, use_cpp = TRUE, which = 
 		}
 	}
 	dims = dim(x) + !as_points
-	if (use_cpp)
-		structure(CPL_xy2sfc(cc, as.integer(dims), as_points, as.integer(which)), 
-			crs = st_crs(xd$refsys), n_empty = 0L, bbox = bbox.Mtrx(cc))
-	else
+	if (use_cpp) {
+		bb = if (cc_has_NAs <- anyNA(cc))
+				bbox.Mtrx(na.omit(cc))
+			else
+				bbox.Mtrx(cc)
+		structure(CPL_xy2sfc(cc, as.integer(dims), as_points, as.integer(which), cc_has_NAs), 
+			crs = st_crs(xd$refsys), n_empty = 0L, bbox = bb)
+	} else
 		st_sfc(xy2sfc(cc, dims, as_points), crs = xd$refsys)
 }
 
