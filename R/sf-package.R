0d52b9b5d3b21486b8ab74821ead85376bdc237a
olivroy <olivierroy71@hotmail.com>
2023-12-08 11:34:13 -0500

    Change default sf-package Rd file + add references

---
+++ R/sf-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+#' @aliases sf-package
+#' @references 
+#' Pebesma, E. and Bivand, R. (2023). Spatial Data
+#'   Science: With Applications in R. Chapman and Hall/CRC.
+#'   <https://doi.org/10.1201/9780429459016> which is also found freely
+#'   online at <https://r-spatial.org/book/>
+#'   
+#' Pebesma, E., 2018. Simple Features for R: Standardized Support
+#'   for Spatial Vector Data. The R Journal 10 (1), 439-446,
+#'   <https://doi.org/10.32614/RJ-2018-009> (open access)
+"_PACKAGE"
