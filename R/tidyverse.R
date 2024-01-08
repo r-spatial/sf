78a921f8785aa7defbb366a34f9ded31d7f65e49
Andy Baxter <andrewbaxter439@gmail.com>
2024-01-08 22:11:47 +0000

    remove renaming function for already renamed names

--- R/tidyverse.R
+++ R/tidyverse.R
@@ -269,8 +269,6 @@ rename_with.sf = function(.data, .fn, .cols, ...) {
 		ret = dplyr::as_tibble(ret)
 	ret = st_as_sf(ret, sf_column_name = names(ret)[sf_column_loc])
 	
-	names(agr) = .fn(names(agr), ...)
-	st_agr(ret) = agr
 	ret
 }
 
