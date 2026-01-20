3dc8a4eac0b9766aa73f1152b235495a0e42438f
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2026-01-20 14:02:19 +0100

    Add include guards to header files
    This speeds up compilation and avoids
    compilation errors from defining
    the same struct/class multiple
    times when a compilation unit gets
    the header included multiple times.

--- src/gdal.h
+++ src/gdal.h
@@ -1,3 +1,5 @@
+#ifndef SF_GDAL_H_
+#define SF_GDAL_H_
 void set_error_handler(void);
 void unset_error_handler(void);
 OGRSpatialReference *handle_axis_order(OGRSpatialReference *sr);
@@ -6,3 +8,4 @@ Rcpp::CharacterVector wkt_from_spatial_reference(const OGRSpatialReference *srs)
 int srid_from_crs(Rcpp::List crs);
 void   set_config_options(Rcpp::CharacterVector ConfigOptions);
 void unset_config_options(Rcpp::CharacterVector ConfigOptions);
+#endif // SF_GDAL_H_
