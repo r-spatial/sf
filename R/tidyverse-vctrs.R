4ffb01dcbea72f8cffc967fc3a7280b08debc336
edzer <edzer.pebesma@uni-muenster.de>
2026-03-06 17:51:27 +0100

    fixes #2589

--- R/tidyverse-vctrs.R
+++ R/tidyverse-vctrs.R
@@ -1,5 +1,6 @@
 types = c("POINT", "MULTIPOINT", "LINESTRING", "MULTILINESTRING", 
-            "POLYGON", "MULTIPOLYGON", "GEOMETRYCOLLECTION", "GEOMETRY")
+            "POLYGON", "MULTIPOLYGON", "GEOMETRYCOLLECTION", "GEOMETRY",
+			"MULTISURFACE")
 sfc_types = paste0("sfc_", types)
 
 # All S3 methods in this file are registered lazily when vctrs is loaded
