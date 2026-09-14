7b95ee067b7cdf5b32e723cd2d26c496d136bfb4
edzer <edzer.pebesma@uni-muenster.de>
2026-09-14 22:30:16 +0200

    name

--- R/aggregate.R
+++ R/aggregate.R
@@ -306,7 +306,7 @@ interpolate_aw_sf_stars = function(x, to, extensive, ..., keep_NA = keep_NA, na.
 	dm = st_dimensions(x)[1:2]
 	dm[["attribute"]] = structure(list(from = 1L, to = length(g), offset = NA_real_, delta = NA_real_,
 						 point = any(st_dimension(g) > 0), values = dimnames(x)[[2]]), class = "dimension")
-	st_as_stars(list(ret), dimensions = st_dimensions(to))
+	st_as_stars(list(attributes = ret), dimensions = st_dimensions(to))
 }
 
 dasymetric = function(x, to, extensive, keep_NA, include_non_intersected) {
